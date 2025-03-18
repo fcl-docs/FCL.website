4c5a6aed1b1f4d55d4abd04a80c62066102d6ea8
XiaoluoFoxington <2046665121@qq.com>
2025-03-19 06:09:00 +0800

    第 1 个提交

---
+++ js/index.js
@@ -0,0 +1,103 @@
+window.onload = function() {
+  
+  hideTip('wo');
+  checkVerticalView();
+  
+};
+
+document.addEventListener('DOMContentLoaded', function() {
+  
+  loadSidebar();
+  hideTip('dom');
+  
+});
+
+window.addEventListener('resize', function() {
+  // checkVerticalView();
+});
+
+function loadSidebar() {
+  const sidebarContainer = document.getElementById('sidebar');
+  const errorHtmlS = '<div class="diagonalRed window"><div class="redT windowTitle"><span>错误</span></div><p>无法加载侧边栏';
+  const errorHtmlE = '</p></div>'
+  
+  if (sidebarContainer) {
+    fetch('/page/content/sidebar.html')
+      .then(response => {
+        if (!response.ok) {
+          throw new Error(`HTTP error! status: ${response.status}`);
+        }
+        return response.text();
+      })
+      .then(html => {
+        sidebarContainer.insertAdjacentHTML('beforeend', html);
+      })
+      .catch(error => {
+        console.error('错误于加载侧边栏:', error);
+        sidebarContainer.insertAdjacentHTML('beforeend', errorHtmlS + '<br>' + error + errorHtmlE);
+      });
+  } else {
+    console.warn('警告于无法找到侧边栏容器');
+  }
+  
+}
+
+function hideTip(tip) {
+  var wot = document.getElementById('windowOnloadtip');
+  var jst = document.getElementById('JStip');
+  var domt = document.getElementById('DOMtip');
+  if (tip === 'wo') {
+    wot.style.display = 'none';
+    jst.style.display = 'none';
+  }
+  if (tip === 'dom') {
+    domt.style.display = 'none';
+  }
+}
+
+function expandSidebar(isExpand) {
+  const sidebarElement = document.querySelector('.sidebar');
+  const mainElement = document.querySelector('.main');
+  const expandButton = document.getElementById('expandSidebar');
+  const contractButton = document.getElementById('contractSidebar');
+  if (isExpand === 'E') {
+    sidebarElement.classList.add('sidebarExpand');
+    mainElement.classList.add('mainExpand');
+    expandButton.style.display = 'none';
+    contractButton.style.display = 'block';
+    console.log('侧边栏展开：是');
+  } else {
+    sidebarElement.classList.remove('sidebarExpand');
+    mainElement.classList.remove('mainExpand');
+    expandButton.style.display = 'block';
+    contractButton.style.display = 'none';
+    console.log('侧边栏展开：否');
+  }
+}
+
+function checkVerticalView() {
+  const viewportHeight = window.innerHeight;
+  const viewportWidth = window.innerWidth;
+  const sidebarElement = document.querySelector('.sidebar');
+  const mainElement = document.querySelector('.main');
+  const sidebarContainer = document.getElementById('sidebar');
+  const tipHTML = '<meta charset="UTF-8"><div class="diagonal window" id="verticalTip"><div class="windowTitle"><span>视口宽度小于视口高度</span></div><p>视口高度较小，可能会导致侧边栏显示不全或异常。<br><button onclick="expandSidebar(&quot;E&quot;)" id="expandSidebar">展开侧边栏</button> <button onclick="expandSidebar(&quot;C&quot;)" id="contractSidebar" style="display: none">收起侧边栏</button></p></div>'
+  
+  console.log('视口高：' + viewportHeight);
+  console.log('视口宽：' + viewportWidth);
+  if (viewportHeight > viewportWidth) {
+    sidebarContainer.insertAdjacentHTML('beforeend', tipHTML);
+    console.log('视口为竖屏：是')
+    // sidebarElement.classList.add('sidebarVertical');
+    // mainElement.classList.add('mainVertical');
+  } else {
+    const tipElement = document.getElementById('verticalTip');
+    if (tipElement) {
+      tipElement.style.display = 'none';
+    }
+    console.log('视口为竖屏：否')
+    // sidebarElement.classList.remove('sidebarVertical');
+    // mainElement.classList.remove('mainVertical');
+  }
+  
+}
